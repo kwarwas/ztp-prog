71707bbdb861fbdb1a67ac8add565827f8257b6b
Kornel Warwas <kornel.warwas@gmail.com>
2016-09-17 00:13:51 +0200

    WaitAll with Main thread

---
+++ tips-and-tricks/TPL/Task_5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Async_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sw = new Stopwatch();
+
+            sw.Start();
+
+            var t1 = Task.Run(() => Method());
+            var t2 = Task.Run(() => Method());
+            var t3 = Task.Run(() => Method());
+
+            Task.WaitAll(t1, t2, t3);
+
+            Console.WriteLine(sw.Elapsed);
+        }
+
+        static void Method()
+        {
+            Task.Delay(TimeSpan.FromSeconds(5)).Wait();
+        }
+    }
+}
