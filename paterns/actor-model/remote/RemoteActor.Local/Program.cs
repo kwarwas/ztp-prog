899531894b0e9d77605724817f1aecc040b41f7c
Kornel Warwas <kornel.warwas@gmail.com>
2016-10-15 23:46:00 +0200

    NEW: Akka.NET Remote

---
+++ paterns/actor-model/remote/RemoteActor.Local/Program.cs
@@ -0,0 +1,26 @@
+﻿using Akka.Actor;
+using RemoteActor.Common.Actors;
+using RemoteActor.Common.Messages;
+using System;
+
+namespace RemoteActor.Local
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var system = ActorSystem.Create("RemoteActor");
+
+            var actor = system.ActorOf(Props.Create<OrderActor>(), "OrderActor");
+
+            for (int i = 1; i <= 5; i++)
+            {
+                actor.Tell(new OrderMessage(i, $"Akka.NET Book {i}"));
+            }
+
+            Console.WriteLine("All messages have been sent");
+
+            system.WhenTerminated.Wait();
+        }
+    }
+}
---
+++ paterns/actor-model/remote/RemoteActor.Remote/Program.cs
@@ -0,0 +1,15 @@
+﻿using Akka.Actor;
+
+namespace ActorStashing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var system = ActorSystem.Create("RemoteActor");
+
+            system.WhenTerminated.Wait();
+        }
+    }
+}
+
